c5fe981a6ec3fa86576209f3f14f154a8c9b8b9a
Aaron Shafovaloff <ashafovaloff@instructure.com>
2014-01-31 16:26:02 -0700

    towards stable

--- index.js
+++ index.js
@@ -1,21 +1,13 @@
 // For examples
 // https://github.com/karma-runner/karma/blob/master/lib/reporters/base.js
-// e
+// https://github.com/karma-runner/karma-junit-reporter/blob/master/index.js
 // https://github.com/karma-runner/karma-growl-reporter/blob/master/index.js
 
 var request = require('request');
 var RSVP = require('rsvp');
-var uuid = require('node-uuid');
-
-var ENV = {
-  FIREWORK_URL: "",
-  GERRIT_PROJECT: "foo",
-  GERRIT_PATCHSET_NUMBER: "1",
-  GERRIT_CHANGE_URL: "WAT",
-  JOB_NAME: "bar",
-  BUILD_NUMBER: "2",
-  BUILD_URL: "YES"
-};
+var QUnitParser = require('qunit-parser');
+var _ = require('lodash');
+var ENV = process.env;
 
 var FireworkReporter = function(baseReporterDecorator, config, logger, helper, formatError) {
   var collector;
@@ -24,18 +16,12 @@ var FireworkReporter = function(baseReporterDecorator, config, logger, helper, f
   baseReporterDecorator(this);
 
   // this.onBrowserStart = function(browser) {
-  //   console.log('onBrowserStart')
-  //   // browser.name
-  //   // browser.id
-  //   // var timestamp = (new Date()).toISOString().substr(0, 19);
   // };
 
   this.onRunStart = function(browsers) {
     suites = Object.create(null);
-    console.log('onRunStart');
 
     collector = {
-      id: uuid.v1(),
       project: ENV.GERRIT_PROJECT,
       results: []
     };
@@ -53,41 +39,31 @@ var FireworkReporter = function(baseReporterDecorator, config, logger, helper, f
       if (ENV.BUILD_NUMBER) collector.build.ci_build_id = ENV.BUILD_NUMBER;
       if (ENV.BUILD_URL) collector.build.url = ENV.BUILD_URL;
     }
-  };
+  }
 
   this.onSpecComplete = function(browser, result) {
-    if(result.log && result.log.length) {
-      result.log.forEach(function(log,index) {
-        result.log[index] = parseLog(log)
-      });
+    var collectorResult = _.pick(result,'success');
+    collectorResult.test = result.description;
+    collectorResult.browser = browser.name;
+    collectorResult.duration_ms = browser.lastResult.netTime;
+    collectorResult.context = result.suite;
+
+    if(!result.success) {
+      var parsedLog = QUnitParser.parseLog(result.log[0]);
+      _.assign(collectorResult,_.pick(parsedLog,'actual','expected','backtrace'));
+      collectorResult.details = parsedLog.description;
     }
 
-    collector.results.push({
-      browser: browser,
-      result: result
-    });
+    collector.results.push(collectorResult);
   };
 
-  this.onBrowserError = function(browser, error) {
-    console.log('browser error');
-  };
+  // this.onBrowserError = function(browser, error) {
+  // };
 
-  this.onBrowserLog = function(browser, log, type) {
-    collector.results.push({
-      browser: browser,
-      log: log,
-      type: type
-    });
-  };
-
-  this.specSuccess = this.specSkipped = this.specFailure = function(browser, result) {
-    console.log('noooooo')
-  //   console.log(JSON.stringify(browser));
-  //   console.log(JSON.stringify(result));
-  };
+  // this.onBrowserLog = function(browser, log, type) {
+  // };
 
   this.postResults = function() {
-    console.log(JSON.stringify(collector,null,2));
     var promise = new RSVP.Promise(function(resolve,reject) {
       request({
         method: 'POST',
@@ -102,12 +78,10 @@ var FireworkReporter = function(baseReporterDecorator, config, logger, helper, f
   };
 
   this.onRunComplete = function() {
-    // console.log('[FIREWORK] sending ' + collector.results.length + ' results to firework');
     queue.then(this.postResults);
   };
 
   this.onExit = function(done) {
-    console.log('onExit');
     queue.then(done);
   };
 };
@@ -117,40 +91,3 @@ FireworkReporter.$inject = ['baseReporterDecorator', 'config', 'logger', 'helper
 module.exports = {
   'reporter:firework': ['type', FireworkReporter]
 };
-
-function extractTrace(lines) {
-  var traceLines = [];
-  while(lines.length && lines[lines.length - 1].trim().indexOf('at ') === 0) {
-     traceLines.push(lines.pop());
-  }
-  return traceLines;
-}
-
-function extractSection(text,lines) {
-  for(var x = 0;x < lines.length;x++) {
-    if(lines[x].indexOf(text) === 0) {
-      lines[x] = lines[x].substr(text.length);
-      return lines.splice(x);
-    }
-  }
-  return [];  
-}
-
-function parseLog(value) {
-  var data = {};
-  
-  var lines = value.match(/[^\r\n]+/g);
-  
-  var traceLines = extractTrace(lines);
-  if(traceLines) data.stack = traceLines;
-  
-  var actual = extractSection('Actual: ',lines);
-  if(actual.length) data.actual = actual;
-  
-  var expected = extractSection('Expected: ',lines);
-  if(expected.length) data.expected = expected;
-  
-  if(lines.length) data.description = lines;
-  
-  return data;
-}
